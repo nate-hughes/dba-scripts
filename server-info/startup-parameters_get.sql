116507563c0ead060009b85f83618f4d1b927ff4
Nate Hughes <nhughes1246@gmail.com>
2017-09-20 14:53:04 -0400

    Initial commit

---
+++ server-info/startup-parameters_get.sql
@@ -0,0 +1,21 @@
+-- http://blog.waynesheffield.com/wayne/archive/2017/09/registry-sql-server-startup-parameters/
+
+DECLARE @RegHive    VARCHAR(50),
+        @RegKey     VARCHAR(100);
+ 
+SET @RegHive = 'HKEY_LOCAL_MACHINE';
+SET @RegKey  = 'Software\Microsoft\MSSQLSERVER\MSSQLServer\Parameters';
+ 
+-- Get all of the arguments / parameters when starting up the service.
+DECLARE @SQLArgs TABLE (
+    Value   VARCHAR(50),
+    Data    VARCHAR(500),
+    ArgNum  AS CONVERT(INTEGER, REPLACE(Value, 'SQLArg', '')));
+ 
+INSERT INTO @SQLArgs
+    EXECUTE master.sys.xp_instance_regenumvalues @RegHive, @RegKey;
+ 
+SELECT * FROM @SQLArgs;
+
+DBCC TRACESTATUS;
+
