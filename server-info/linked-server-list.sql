5ba3cda0fd8cf969ce53f11b63c2b1ec2e8ed181
Nate Hughes <nhughes1246@gmail.com>
2017-10-03 13:22:38 -0400

    Review and add scripts 10032017

---
+++ server-info/linked-server-list.sql
@@ -0,0 +1,19 @@
+
+
+SELECT	[SrvId] = s.server_id
+		, [SrvEnabled] = s.is_data_access_enabled
+		, [SrvName] = s.name
+		, [SrvProduct] = s.product
+		, [SrvProvider] = s.provider
+		, [SrvDataSource] = s.data_source
+		, [LocalLogin] = ISNULL(c.name,'')
+		, [RemoteLogin]	= ISNULL(ll.remote_name,'')
+		, [OutgoingRPCEnabled] = s.is_rpc_out_enabled
+		, [DTCEnabled] = s.is_remote_proc_transaction_promotion_enabled
+FROM	sys.Servers s
+		LEFT OUTER JOIN sys.linked_logins ll ON ll.server_id = s.server_id
+		LEFT OUTER JOIN sys.server_principals c ON c.principal_id = ll.local_principal_id
+WHERE	s.server_id > 0
+ORDER BY s.name;
+
+--exec sp_linkedservers
